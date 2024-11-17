96463fcf04cab32fc945008bf27831e2cbeffde0
aquintanab <nanaquintana21@gmail.com>
2024-11-17 11:19:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.lottie(animation, width=350)
 
 st.write("Toca el Botón y habla")
 
-stt_button = Button("Habla",icon="🎙️", width=200)
+stt_button = Button("Habla🎙️", width=200)
 
 stt_button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
